70dcf75dba5c842c1ed1046e85e0934f40f3c9a5
Sam Lee <Leeabc@SamdeMacBook-Pro.local>
2016-04-08 00:28:04 +0800

    upgrade to RN 2.3

--- index.ios.js
+++ index.ios.js
@@ -3,10 +3,7 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-import React from 'react-native';
+import React,{ AppRegistry } from 'react-native';
 import App from './react/views/app';
-var {
-  AppRegistry,
-} = React;
 
 AppRegistry.registerComponent('Splace', () => App);
